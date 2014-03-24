188c35f866d520dbb25b6d0a4d0b57aaac75fdab
Søren Louv-Jansen <sorenlouv@gmail.com>
2014-03-24 01:52:32 -0700

    Added sort logic to utils

--- utils.js
+++ utils.js
@@ -30,13 +30,21 @@ exports.parseParameters = function(reqParams){
   var offset = reqParams.offset || 0;
   var limit = reqParams.limit || 10;
 
+  var sort = {};
+  if(reqParams.sort){
+    var sort_order = isNaN(Number(reqParams.sort_order)) ? 1 : Number(reqParams.sort_order);
+    sort[reqParams.sort] = sort_order;
+  }
+
   var query = extend({}, reqParams); // Clone
   delete query.offset;
   delete query.limit;
+  delete query.sort;
 
   return {
     query: query,
     offset: offset,
-    limit: limit
+    limit: limit,
+    sort: sort
   };
 };
